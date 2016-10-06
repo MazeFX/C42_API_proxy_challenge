8858b7a2c1141684ad72ae0aa01a33f255395556
MazeFX <filoplast@gmail.com>
2016-10-06 12:50:11 +0200

    Lunch break is over: saving all changes so far

--- api_requests.py
+++ api_requests.py
@@ -12,8 +12,43 @@ into new a new json object, caching the object and returning it as a response.
 
 """
 
+import environ
 from django.http import JsonResponse
+from django.core.cache import cache
+
+env = environ.Env()
+
+
+def create_json_response_object(event_id):
+	base_url = 'https://demo.calendar42.com/api/v2/'
+	api_token = env('C42_API_TOKEN', default=None)
+        if api_token is None:
+            raise ValueError('Critical Error: No C42_API_TOKEN environment variable set!')
+
+	babelfish = Babelfish(base_url, api_token)
+
+    event_json = babelfish.get_event(event_id).json()
+    event_subscriptions_json = babelfish.get_event_subscriptions(event_id).json()
+
+    name_list = [x['subscriber']['first_name'] for x in event_subscriptions_json["data"]]
+
+    json_object = {"id": event_response_json["data"][0]["id"],
+                   "title": event_response_json["data"][0]["title"],
+                   "names": name_list}
+
+    return json_object
+
+
+def get_response_json(event_id):
+	response_json = cache.get('ews_{}'.format(event_id))
+
+	if response_json is None:
+		response_json = create_json_response_object(event_id)
+		cache.set('ews_{}'.format(event_id), 252)
+
+	return response_json
 
 
 def get_events_with_subscriptions(request, event_id):
-    return JsonResponse({'id': event_id})
+	response = get_response_json(event_id)
+    return JsonResponse(response)
--- tests/test_api_requests.py
+++ tests/test_api_requests.py
@@ -13,11 +13,17 @@ Provides tests for correctly directing the incoming requests according to proxy 
 
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from unittest.mock import patch, MagicMock
 
 from C42_API_proxy_challenge.api_requests import get_events_with_subscriptions
 
 
 class C42RequestsTest(TestCase):
+
+	def setUp(self):
+        patcher_cache = patch('C42_API_proxy_challenge.api_requests.cache')
+        self.mock_cache = patcher_cache.start()
+        self.addCleanup(patcher_cache.stop)
 
     def test_event_with_subscriptions_url_resolves_to_api_proxy(self):
         found = resolve('/C42/events-with-subscriptions/event_id/')
@@ -27,3 +33,21 @@ class C42RequestsTest(TestCase):
         event_id = 'd6e66cb0ced8e46102bcfd93ceac51b0_14752373875438'
         response = self.client.get('/C42/events-with-subscriptions/{}/'.format(event_id))
         self.assertEqual(response.json()['id'], event_id)
+
+    def test_do_something(self):
+        """
+        When do_something is called, cache.set is called exactly one
+        """
+        mymodel = MyModel()
+        mymodel.do_something()
+        self.assertEqual(self.mock_cache.set.call_count, 1)
+
+    def test_caching_layer(self):
+        self.cache.set(key='marco', value='polo')
+
+        with mock.patch.object(SomeClass, 'workhorse_function') as mocked_method:
+            sc = SomeClass()
+            sc.get_by_username('marco')
+
+            if mocked_method.called:
+                self.fail("This was supposed to be a cache hit")
