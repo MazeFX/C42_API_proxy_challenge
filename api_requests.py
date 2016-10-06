6cbc55903d719ac0542865c76a3c2f7f28f2eab8
MazeFX <filoplast@gmail.com>
2016-10-06 21:30:01 +0200

    Passing tests for caching the response for the initial call.

--- api_requests.py
+++ api_requests.py
@@ -16,39 +16,41 @@ import environ
 from django.http import JsonResponse
 from django.core.cache import cache
 
+from .babelfish import Babelfish
+
 env = environ.Env()
 
 
 def create_json_response_object(event_id):
-	base_url = 'https://demo.calendar42.com/api/v2/'
-	api_token = env('C42_API_TOKEN', default=None)
-        if api_token is None:
-            raise ValueError('Critical Error: No C42_API_TOKEN environment variable set!')
+    base_url = 'https://demo.calendar42.com/api/v2/'
+    api_token = env('C42_API_TOKEN', default=None)
+    if api_token is None:
+        raise ValueError('Critical Error: No C42_API_TOKEN environment variable set!')
 
-	babelfish = Babelfish(base_url, api_token)
+    babelfish = Babelfish(base_url, api_token)
 
     event_json = babelfish.get_event(event_id).json()
     event_subscriptions_json = babelfish.get_event_subscriptions(event_id).json()
 
     name_list = [x['subscriber']['first_name'] for x in event_subscriptions_json["data"]]
 
-    json_object = {"id": event_response_json["data"][0]["id"],
-                   "title": event_response_json["data"][0]["title"],
+    json_object = {"id": event_json["data"][0]["id"],
+                   "title": event_json["data"][0]["title"],
                    "names": name_list}
 
     return json_object
 
 
 def get_response_json(event_id):
-	response_json = cache.get('ews_{}'.format(event_id))
+    response_json = cache.get('ews_{}'.format(event_id))
 
-	if response_json is None:
-		response_json = create_json_response_object(event_id)
-		cache.set('ews_{}'.format(event_id), 252)
+    if response_json is None:
+        response_json = create_json_response_object(event_id)
+        cache.set('ews_{}'.format(event_id), response_json, 252)
 
-	return response_json
+    return response_json
 
 
 def get_events_with_subscriptions(request, event_id):
-	response = get_response_json(event_id)
+    response = get_response_json(event_id)
     return JsonResponse(response)
--- tests/test_api_requests.py
+++ tests/test_api_requests.py
@@ -15,39 +15,64 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from unittest.mock import patch, MagicMock
 
-from C42_API_proxy_challenge.api_requests import get_events_with_subscriptions
+from C42_API_proxy_challenge.api_requests import (get_events_with_subscriptions,
+    get_response_json, create_json_response_object)
 
 
 class C42RequestsTest(TestCase):
 
-	def setUp(self):
+    def setUp(self):
         patcher_cache = patch('C42_API_proxy_challenge.api_requests.cache')
         self.mock_cache = patcher_cache.start()
         self.addCleanup(patcher_cache.stop)
+
+        patcher_get_response_json = patch('C42_API_proxy_challenge.api_requests.get_response_json')
+        self.mock_get_response_json = patcher_get_response_json.start()
+        self.addCleanup(patcher_get_response_json.stop)
+
+        patcher_create_json_response_object = patch('C42_API_proxy_challenge.api_requests.create_json_response_object')
+        self.mock_create_json_response_object = patcher_create_json_response_object.start()
+        self.addCleanup(patcher_create_json_response_object.stop)
+
+        self.test_event_id = 'DisasterAreaConcert'
 
     def test_event_with_subscriptions_url_resolves_to_api_proxy(self):
         found = resolve('/C42/events-with-subscriptions/event_id/')
         self.assertEqual(found.func, get_events_with_subscriptions)
 
     def test_C42_proxy_get_request_returns_json_object(self):
-        event_id = 'd6e66cb0ced8e46102bcfd93ceac51b0_14752373875438'
-        response = self.client.get('/C42/events-with-subscriptions/{}/'.format(event_id))
-        self.assertEqual(response.json()['id'], event_id)
+        self.mock_get_response_json.return_value = {'event_id': 'DisasterAreaConcert'}
+        response = self.client.get('/C42/events-with-subscriptions/{}/'.format(self.test_event_id))
+        self.assertEqual(response.json()['event_id'], self.test_event_id)
 
-    def test_do_something(self):
-        """
-        When do_something is called, cache.set is called exactly one
-        """
-        mymodel = MyModel()
-        mymodel.do_something()
+    def test_get_response_json(self):
+        self.mock_cache.get.return_value = None
+        self.mock_create_json_response_object.return_value = {'event_id': 'DisasterAreaConcert'}
+
+        response_json = get_response_json(self.test_event_id)
+        self.assertEqual(response_json, {'event_id': 'DisasterAreaConcert'})
+
+    def test_if_get_response_json_is_cached(self):
+        self.mock_cache.get.return_value = None
+        self.mock_create_json_response_object.return_value = {'event_id': 'DisasterAreaConcert'}
+
+        response_json = get_response_json(self.test_event_id)
         self.assertEqual(self.mock_cache.set.call_count, 1)
+        self.mock_cache.set.assert_called_once_with('ews_{}'.format(self.test_event_id),
+                                                    {'event_id': 'DisasterAreaConcert'}, 252)
 
-    def test_caching_layer(self):
-        self.cache.set(key='marco', value='polo')
+    def test_if_get_response_json_gets_returned_from_cache(self):
+        self.mock_cache.get.return_value = {'event_id': 'DisasterAreaConcert'}
 
-        with mock.patch.object(SomeClass, 'workhorse_function') as mocked_method:
-            sc = SomeClass()
-            sc.get_by_username('marco')
+        response_json = get_response_json(self.test_event_id)
+        self.assertEqual(self.mock_cache.set.call_count, 0)
 
-            if mocked_method.called:
-                self.fail("This was supposed to be a cache hit")
+    # def test_caching_layer(self):
+    #     self.cache.set(key='marco', value='polo')
+    #
+    #     with mock.patch.object(SomeClass, 'workhorse_function') as mocked_method:
+    #         sc = SomeClass()
+    #         sc.get_by_username('marco')
+    #
+    #         if mocked_method.called:
+    #             self.fail("This was supposed to be a cache hit")
